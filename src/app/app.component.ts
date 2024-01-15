abbe6193a3ee32231c55c1ddc1f2c49b69df1897
Denis Kuznetsov <denis.kyznecov@gmail.com>
2024-01-15 10:44:58 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  // templateUrl: './app.component.html',
+  template: `
+    <p>hello world</p>
+    <div class="buttons">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+    </div>
+  `,
+
+  // styleUrl: './app.component.scss',
+  styles: `
+    .buttons{
+      display: flex;
+      gap: 20px;
+    }
+  `,
 })
 export class AppComponent {
   title = 'ngxsMy';
